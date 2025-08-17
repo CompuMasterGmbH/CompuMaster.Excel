83cd2f218e4798bb0ff2eca93841360bf15a626b
Jochen Wezel <jwezel@compumaster.de>
2025-08-17 17:37:21 +0200

    ignore tests if not supported by runtime platform

--- ExcelOpsTest-SpireXls/SpecialFeature_SaveToHtml.vb
+++ ExcelOpsTest-SpireXls/SpecialFeature_SaveToHtml.vb
@@ -18,8 +18,12 @@ Public Class SpecialFeature_SaveToHtml
         System.Console.WriteLine("TEST IN FILE: " & TestXlsxFile.FullName)
         System.Console.WriteLine("TEST OUT FILE: " & TestHtmlOutputFile)
 
-        Dim Wb As New SpireXlsDataOperations(TestXlsxFile.FullName, ExcelOps.ExcelDataOperationsBase.OpenMode.OpenExistingFile, True, Nothing, True)
-        Wb.SaveToHtml(TestHtmlOutputFile, False)
+        Try
+            Dim Wb As New SpireXlsDataOperations(TestXlsxFile.FullName, ExcelOps.ExcelDataOperationsBase.OpenMode.OpenExistingFile, True, Nothing, True)
+            Wb.SaveToHtml(TestHtmlOutputFile, False)
+        Catch ex As TypeInitializationException
+            Assert.Ignore("Not supported on this platform " & System.Environment.OSVersion.Platform.ToString)
+        End Try
 
         If OPEN_OUTPUT_IN_BROWSER_AFTER_TEST Then
             Dim OpenFileProcess = System.Diagnostics.Process.Start(New System.Diagnostics.ProcessStartInfo() With {
@@ -34,7 +38,12 @@ Public Class SpecialFeature_SaveToHtml
         Dim TestXlsxFile = TestFiles.TestFileGrund01()
         System.Console.WriteLine("TEST IN FILE: " & TestXlsxFile.FullName)
 
-        Dim Wb As New SpireXlsDataOperations(TestXlsxFile.FullName, ExcelOps.ExcelDataOperationsBase.OpenMode.OpenExistingFile, True, Nothing, True)
+        Dim Wb As SpireXlsDataOperations = Nothing
+        Try
+            Wb = New SpireXlsDataOperations(TestXlsxFile.FullName, ExcelOps.ExcelDataOperationsBase.OpenMode.OpenExistingFile, True, Nothing, True)
+        Catch ex As TypeInitializationException
+            Assert.Ignore("Not supported on this platform " & System.Environment.OSVersion.Platform.ToString)
+        End Try
         For Each WorkSheetName In Wb.WorkSheetNames
             System.Console.WriteLine("FOUND WORKSHEET: " & WorkSheetName)
         Next
